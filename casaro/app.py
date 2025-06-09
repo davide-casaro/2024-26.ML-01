854ac2c37cd0324f0dcf0823bad73927582eadbf
davide-casaro <davide.casaro@itsrizzoli.it>
2025-06-09 17:25:28 +0200

    artefatto si, ultima parte no

--- casaro/app.py
+++ casaro/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
+
+feature_cols=['release_date', 'publisher', 'median_playtime', 'price', 'Genre: Action', 
+              'Genre: Adventure', 'Genre: Casual', 'Genre: Early Access', 
+              'Genre: Free to Play', 'Genre: Indie', 'Genre: Massively Multiplayer', 
+              'Genre: RPG', 'Genre: Racing', 'Genre: Simulation', 'Genre: Sports', 'Genre: Strategy']
 
 @app.route('/infer', methods=['POST'])
 def hello():
     data = request.get_json()
-    param1 = data.get('param1')
-
-    # mymodel = joblib.load("artefatto")
-    # infer_result = mymodel.predict(param1)
+    param1 = data.get("param1", "")
+    
+    mymodel = joblib.load("mymodel.joblib")
+    infer_result = mymodel.predict(param1)
 
     response_data = {
         "result": {
